017dbd0267cbb9af4366f61d183a85e74f186d96
rangapin <rangapin@gmx.com>
2022-12-21 20:49:29 -0600

    updated color panel

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,10 @@ module.exports = {
     },
     extend: {
       colors: {
-        strongCyan: 'hsl(171, 66%, 44%)',
-        lightBlue: 'hsl(233, 100%, 69%)',
-        darkGrayishBlue: 'hsl(210, 10%, 33%)',
-        grayishBlue: 'hsl(201, 11%, 66%)',
+        strongCyan: '#1E90FF',
+        lightBlue: '#4169E1',
+        darkGrayishBlue: '#4682B4',
+        grayishBlue: '#B0C4DE',
       },
       fontFamily: {
         sans: ['Bai Jamjuree', 'sans-serif'],
